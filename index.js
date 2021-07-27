d5930ac6548dc38dcfc03b6d858565143bb2c0bc
Ethan Chiu <ethanchewy@github.com>
2021-07-26 17:16:53 -0700

    Add test for emojii generation

--- index.js
+++ index.js
@@ -7,13 +7,12 @@ async function run() {
   try {
     const emojii_list_string = core.getInput('list_of_emojiis');
     const gh_repo = core.getInput('gh_base_url');
-    const octokit = getOctokit(token);
 
     // Select random emojii from list
-    const chosen_emojii = emojii_list[Math.floor(Math.random() * emojii_list_string.length)]
+    const chosen_emojii = emojii_list_string[Math.floor(Math.random() * emojii_list_string.length)];
 
     // Construct base_url for emojii
-    let base_url =  "https://github.com/" + gh_repo + "/issues/new?title=emojii%7C" + chosen_emojii + "&body=Just+click+%27Submit+new+issue%27.+That+is+it."
+    let base_url =  "https://github.com/" + gh_repo + "/issues/new?title=emojii%7C" + chosen_emojii + "&body=Just+click+%27Submit+new+issue%27.+That+is+it.";
 
     core.setOutput('random_url', base_url);
   } catch (error) {
--- index.test.js
+++ index.test.js
@@ -1,23 +1,12 @@
-const wait = require('./wait');
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
+  process.env['INPUT_LIST_OF_EMOJIIS'] = '🍎🌶🍜🏀🏆😀😱😤';
+  process.env['INPUT_GH_BASE_URL'] = 'ethanchewy/ethanchewy';
   const ip = path.join(__dirname, 'index.js');
+
   console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
 })
