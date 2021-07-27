590ad2196ff7b03663a258888a344120e2fc43b0
Ethan Chiu <ethanchewy@github.com>
2021-07-27 12:58:55 -0700

    Make sure emojii doesn't get chopped off

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const emojii_list_string = core.getInput('list_of_emojiis');
+    let emojii_list_string = core.getInput('list_of_emojiis');
+    const emojii_list_arr = emojii_list_string.split(" ");
     const gh_repo = core.getInput('gh_base_url');
 
     // Select random emojii from list
-    const chosen_emojii = emojii_list_string[Math.floor(Math.random() * emojii_list_string.length)];
+    const chosen_emojii = emojii_list_arr[Math.floor(Math.random() * emojii_list_arr.length)];
 
     // Construct base_url for emojii
     let base_url =  "https://github.com/" + gh_repo + "/issues/new?title=emojii%7C" + chosen_emojii + "&body=Just+click+%27Submit+new+issue%27.+That+is+it.";
--- index.test.js
+++ index.test.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_LIST_OF_EMOJIIS'] = '🍎🌶🍜🏀🏆😀😱😤';
+  process.env['INPUT_LIST_OF_EMOJIIS'] = '🍎 🌶 🍜 🏀 🏆 😀 😱 😤';
   process.env['INPUT_GH_BASE_URL'] = 'ethanchewy/ethanchewy';
   const ip = path.join(__dirname, 'index.js');
 
